3c31b753298637612b005a0584cdc5bfbb812451
Antonio Chavez Lopez <alu0101038377@ull.edu.es>
2018-11-07 00:06:53 +0000

    Prueba: grasas pliinsaturadas

--- spec/etiqueta_nutricional_spec.rb
+++ spec/etiqueta_nutricional_spec.rb
@@ -47,5 +47,11 @@ RSpec.describe InfoEtiquetaNutricional do
 			expect(28.5).to eq(y.gr_monoinsaturadas)
 		end
 
+		it "Se obtiene las grasas poliinsaturadas del producto" do
+			expect("no_declarado").to eq (@x.gr_poliinsaturadas)
+			y = InfoEtiquetaNutricional.new("nombre",80.2,13.8,0.5,0,0.8,0,28.5,34.6)
+			expect(34.6).to eq(y.gr_poliinsaturadas)
+		end
+
 	end
 end
