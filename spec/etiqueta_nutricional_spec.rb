2a51e9582811008a917788a2317a734038b8421e
Antonio Chavez Lopez <alu0101038377@ull.edu.es>
2018-11-07 00:24:40 +0000

    Prueba: almidón

--- spec/etiqueta_nutricional_spec.rb
+++ spec/etiqueta_nutricional_spec.rb
@@ -59,5 +59,11 @@ RSpec.describe InfoEtiquetaNutricional do
 			expect(0).to eq(y.polialcoholes)
 		end
 
+		it "Se obtiene el almidón del producto" do
+			expect("no_declarado").to eq (@x.almidon)
+			y = InfoEtiquetaNutricional.new("nombre",80.2,13.8,0.5,0,0.8,0,28.5,34.6,0,0)
+			expect(0).to eq(y.almidon)
+		end
+
 	end
 end
