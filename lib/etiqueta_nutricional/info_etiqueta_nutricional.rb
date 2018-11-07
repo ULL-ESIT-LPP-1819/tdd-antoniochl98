2c42bdeeafe0029870b9eb43c9035aba5a802465
Antonio Chavez Lopez <alu0101038377@ull.edu.es>
2018-11-07 00:01:59 +0000

    Prueba superada

--- lib/etiqueta_nutricional/info_etiqueta_nutricional.rb
+++ lib/etiqueta_nutricional/info_etiqueta_nutricional.rb
@@ -1,6 +1,6 @@
 class InfoEtiquetaNutricional
-	attr_reader :nombre, :grasas, :gr_saturadas, :hidratos, :azucar, :proteinas, :sal
-	def initialize(nombre,grasas,gr_saturadas,hidratos,azucar,proteinas,sal)
-		@nombre, @grasas, @gr_saturadas, @hidratos, @azucar, @proteinas, @sal=nombre, grasas, gr_saturadas, hidratos, azucar, proteinas, sal
+	attr_reader :nombre, :grasas, :gr_saturadas, :hidratos, :azucar, :proteinas, :sal, :gr_monoinsaturadas
+	def initialize(nombre,grasas,gr_saturadas,hidratos,azucar,proteinas,sal,gr_monoinsaturadas="no_declarado")
+		@nombre, @grasas, @gr_saturadas, @hidratos, @azucar, @proteinas, @sal, @gr_monoinsaturadas=nombre, grasas, gr_saturadas, hidratos, azucar, proteinas, sal, gr_monoinsaturadas
 	end
 end
