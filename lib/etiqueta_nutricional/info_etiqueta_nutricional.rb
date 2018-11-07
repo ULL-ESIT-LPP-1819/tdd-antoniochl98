c6ea36e7aac4bd555b75f404a930ef5b03e27702
Antonio Chavez Lopez <alu0101038377@ull.edu.es>
2018-11-07 00:27:10 +0000

    Prueba superada

--- lib/etiqueta_nutricional/info_etiqueta_nutricional.rb
+++ lib/etiqueta_nutricional/info_etiqueta_nutricional.rb
@@ -1,6 +1,6 @@
 class InfoEtiquetaNutricional
-	attr_reader :nombre, :grasas, :gr_saturadas, :hidratos, :azucar, :proteinas, :sal, :gr_monoinsaturadas, :gr_poliinsaturadas, :polialcoholes
-	def initialize(nombre,grasas,gr_saturadas,hidratos,azucar,proteinas,sal,gr_monoinsaturadas="no_declarado",gr_poliinsaturadas="no_declarado",polialcoholes="no_declarado")
-		@nombre, @grasas, @gr_saturadas, @hidratos, @azucar, @proteinas, @sal, @gr_monoinsaturadas, @gr_poliinsaturadas,@polialcoholes=nombre, grasas, gr_saturadas, hidratos, azucar, proteinas, sal, gr_monoinsaturadas, gr_poliinsaturadas, polialcoholes
+	attr_reader :nombre, :grasas, :gr_saturadas, :hidratos, :azucar, :proteinas, :sal, :gr_monoinsaturadas, :gr_poliinsaturadas, :polialcoholes, :almidon
+	def initialize(nombre,grasas,gr_saturadas,hidratos,azucar,proteinas,sal,gr_monoinsaturadas="no_declarado",gr_poliinsaturadas="no_declarado",polialcoholes="no_declarado",almidon="no_declarado")
+		@nombre,@grasas,@gr_saturadas,@hidratos,@azucar,@proteinas,@sal,@gr_monoinsaturadas,@gr_poliinsaturadas,@polialcoholes,@almidon=nombre,grasas,gr_saturadas,hidratos,azucar,proteinas,sal,gr_monoinsaturadas,gr_poliinsaturadas,polialcoholes,almidon
 	end
 end
